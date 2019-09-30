ccff4dccf023eeb43fa1f2fabe75a76d66f4a6bb
Ing. Michal Svamberg <svamberg@civ.zcu.cz>
2019-09-30 15:06:05 +0200

    First version check of gluster pool list

---
+++ check_gluster_pool_list.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+MSG_OK="All pools are connected."
+MSG_WARNING="Not all pools are connected."
+MSG_CRITICAL="Not all pools are connected."
+MSG_UNKNOWN="Gluster mischmatch"
+
+STATE=$STATE_OK
+MSG=$MSG_OK
+
+
+LIST=`sudo gluster pool list`
+
+
+FAILS=`echo $RET | grep -v Connected`
+
+if [ ! -z $FAILS ] ; then
+    MSG="$MSG_CRITICAL\n$LIST"
+    STATE=$STATE_CRITICAL
+fi
+
+
+echo "$MSG"
+exit $STATE
+
+
